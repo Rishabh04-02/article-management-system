2ad0ee4744a77b8139ff6140f77063bd5157d18f
Rishabh04-02 <rishabh0402@gmail.com>
2016-05-30 08:20:50 +0530

    final commit

--- credits.php
+++ credits.php
@@ -18,25 +18,8 @@
 			<td><h1>Credits for Project - Article Management System</h1></td>
 
 	</tr>
-	<tr>
 	<td>1.</td>
-		<td><a title="Saumya Sahoo - Faculty Coordinator">Saumya Sahoo - Faculty Coordinator</a></td>
-	</tr>
-	<tr>
-	<td>2.</td>
-		<td>14MI506 - Vidushi Aggarwal</td>
-	</tr>
-	<td>3.</td>
-		<td>14MI507 - Amandeep Prasad</td>
-	</tr>
-	<td>4.</td>
 		<td>14MI508 - Rishabh Chaudhary</td>
-	</tr>
-	<td>5.</td>
-		<td>14MI509 - Chanderkanta Bhatti</td>
-	</tr>
-	<td>6.</td>
-		<td>14MI510 - Abhishek Kumar</td>
 	</tr>
 	
 </table>
