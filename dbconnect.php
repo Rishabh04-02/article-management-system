9ec6433dd0b1d48429a4031b61a430e6f7f76f68
Rishabh Chaudhary <rishabh0402@gmail.com>
2018-06-28 17:36:20 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,11 +1,11 @@
 <?php
     error_reporting( E_ALL & ~E_DEPRECATED & ~E_NOTICE );
 	
-	if(!mysql_connect("localhost","root","hey"))
+	if(!mysql_connect("hostname","username","password"))
 {
 	die('can not connect to server '.mysql_error());
 }
-if(!mysql_select_db("article-management-system"))
+if(!mysql_select_db("database_name"))
 {
 	die('database connection problem '.mysql_error());
 }
